6b02f3f90215a018aa711f40f332f7e880475fd5
Nitzan <nitzan16001@gmail.com>
2023-05-20 20:23:57 +0300

    Connected OpenAI API

--- pages/post/new.js
+++ pages/post/new.js
@@ -1,24 +1,74 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { AppLayout } from "../../components/AppLayout";
+import { useState } from "react";
 
 export default function NewPost(props) {
-    console.log('NEW POST PROPS: ', props);
-    return (
+  console.log("NEW POST PROPS: ", props);
+  const [topic, setTopic] = useState("");
+  const [keywords, setKeyWords] = useState("");
+  const [postContent, setPostContent] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("/api/generatePost", {
+      method: "POST",
+      headers: {
+        'content-type' : 'application/json',
+      },
+      body: JSON.stringify({ topic, keywords}),
+
+    });
+
+    const json = await response.json();
+    console.log("Result: ", json.post.postContent);
+    setPostContent(json.post.postContent);
+  };
+
+  return (
     <div>
-      <h1>homepage</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            <strong>Generate a blog post on the topic of:</strong>
+          </label>
+          <textarea
+            className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+            value={topic}
+            onChange={(e) => setTopic(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>
+            <strong>Targeting the following keywords:</strong>
+          </label>
+          <textarea
+            className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+            value={keywords}
+            onChange={(e) => setKeyWords(e.target.value)}
+          />
+        </div>
+
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+
+      <div
+        className="max-w-screen-sm p-10"
+        dangerouslySetInnerHTML={{ __html: postContent }}
+      />
     </div>
-    );
+  );
 }
 
 NewPost.getLayout = function getLayout(page, pageProps) {
-    return <AppLayout {...pageProps}>{page}</AppLayout>
-}
-  
+  return <AppLayout {...pageProps}>{page}</AppLayout>;
+};
+
 // Runs server side when the page is run. To render props to the page component
 export const getServerSideProps = withPageAuthRequired(() => {
-
-    return {
-        props: {},
-    };
-
-  });
+  return {
+    props: {},
+  };
+});
